89a39c6dd57619fd80083434e7120920926c712f
Muarem <s.muarem10@gmail.com>
2024-06-05 23:27:19 +0300

    created item component

---
+++ components/item.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import React from "react";
+import { style } from "@/app/index.style";
+
+const item = ({ data, images, image }: any) => {
+  return (
+    <View style={style.innerContainer}>
+      <Image style={style.img} source={images[image]} />
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={style.title}>{item.title}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
+
+export default item;
+
+const styles = StyleSheet.create({});
