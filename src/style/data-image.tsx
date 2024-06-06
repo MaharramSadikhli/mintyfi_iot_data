fb27a1ba1017203e90b7b22e847f829e7838bc16
Muarem <s.muarem10@gmail.com>
2024-06-06 10:51:34 +0300

    created data img style component

---
+++ src/style/data-image.tsx
@@ -0,0 +1,8 @@
+import { Colors } from "@/constants/Colors"
+import { ImageStyle, } from "react-native"
+
+export const DataImage: ImageStyle = {
+    width: 40,
+    height: 40,
+    resizeMode: "contain"
+}
