3228b6b2c01e5277d2698240fc9ebd7379f9de19
Muarem <s.muarem10@gmail.com>
2024-06-06 13:34:58 +0300

    Items splited

--- components/items.tsx
+++ components/items.tsx
@@ -3,12 +3,19 @@ import React from 'react'
 import { style } from "@/app/index.style";
 import { Temprature, Drop, Wind, Bulb, Users, WiFi } from './items.data';
 
-const Items = ({data}: any) => {
+const ItemsFirst = ({data}: any) => {
   return (
     <ScrollView horizontal={true} contentContainerStyle={style.rowContainer} >
         <Temprature data={data} />
         <Drop data={data} />
         <Wind data={data} />
+      </ScrollView>
+  )
+}
+
+const ItemsSecond = ({data}: any) => {
+  return (
+    <ScrollView horizontal={true} contentContainerStyle={style.rowContainer} >
         <Bulb data={data} />
         <Users data={data} />
         <WiFi data={data} />
@@ -16,6 +23,6 @@ const Items = ({data}: any) => {
   )
 }
 
-export default Items
+export { ItemsFirst, ItemsSecond }
 
 const styles = StyleSheet.create({})
