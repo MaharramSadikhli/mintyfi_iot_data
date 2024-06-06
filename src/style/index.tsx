eba8499e307400328841dc68e3290896ad50858e
Muarem <s.muarem10@gmail.com>
2024-06-06 17:18:59 +0300

    updated style binding

--- src/style/index.tsx
+++ src/style/index.tsx
@@ -1,9 +1,9 @@
 import { Container } from "./container";
 import { RowContainer } from "./row-container";
-import { InnerContainer } from "./inner-container";
+import { InnerContainer, ListInnerContainer } from "./inner-container";
 import { DataText } from "./data-text";
 import { DataImage } from "./data-image";
 import { LogoContainer } from "./logo-container";
 import { LogoImage } from "./logo-img";
 
-export { Container, RowContainer, InnerContainer, DataText, DataImage, LogoContainer, LogoImage };
+export { Container, RowContainer, InnerContainer, ListInnerContainer, DataText, DataImage, LogoContainer, LogoImage };
