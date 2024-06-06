24487b5973c8c91f4fe0c926cfa67ad100b75354
Muarem <s.muarem10@gmail.com>
2024-06-06 13:29:32 +0300

    row container style refactored

--- src/style/row-container.tsx
+++ src/style/row-container.tsx
@@ -3,4 +3,9 @@ import { ViewStyle } from "react-native"
 
 export const RowContainer: ViewStyle = {
     flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    marginTop: 1,
 }
