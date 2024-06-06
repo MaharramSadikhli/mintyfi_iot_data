dc1548b249a472a791ec42a91a76ac3188f5a247
Muarem <s.muarem10@gmail.com>
2024-06-06 13:27:45 +0300

    data text style refactored

--- src/style/data-text.tsx
+++ src/style/data-text.tsx
@@ -2,7 +2,9 @@ import { Colors } from "@/constants/Colors"
 import { TextStyle } from "react-native"
 
 export const DataText: TextStyle = {
-    fontSize: 8,
-    color: Colors.light.text,
+    fontSize: 12,
+    color: Colors.dark.text,
     fontWeight: "thin",
+    textAlign: "center",
+    marginTop: 2
 }
