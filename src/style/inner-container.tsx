6785685bed6611ce6c777990eb0922266ab4e40d
Muarem <s.muarem10@gmail.com>
2024-06-06 10:37:30 +0300

    created inner container style component

---
+++ src/style/inner-container.tsx
@@ -0,0 +1,9 @@
+import { Colors } from "@/constants/Colors"
+import { ViewStyle } from "react-native"
+
+export const InnerContainer: ViewStyle = {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.light.background
+}
