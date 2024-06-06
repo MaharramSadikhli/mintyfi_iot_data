8deebb6b8f04b07606f0c398142a48e5685c0e9e
Muarem <s.muarem10@gmail.com>
2024-06-06 13:32:16 +0300

    created logo container component

---
+++ src/style/logo-container.tsx
@@ -0,0 +1,10 @@
+import { Colors } from "@/constants/Colors";
+import { ViewStyle } from "react-native";
+
+export const LogoContainer: ViewStyle = {
+  backgroundColor: Colors.light.background,
+  width: "100%",
+  height: "10%",
+  justifyContent: "center",
+  alignItems: "center",
+};
