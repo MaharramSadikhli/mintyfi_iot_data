6675e3dd86e59e25c409510551454ec1cb1bdc89
Muarem <s.muarem10@gmail.com>
2024-06-06 13:35:23 +0300

    refactored items.data.tsx

--- components/items.data.tsx
+++ components/items.data.tsx
@@ -9,60 +9,57 @@ const images = {
   wind: require("../src/img/wind.png"),
   bulb: require("../src/img/bulb.png"),
   users: require("../src/img/users.png"),
-  wifi: require("../src/img/wifi.png")
+  wifi: require("../src/img/wifi.png"),
 };
-
 
 const Temprature = ({ data }: any) => {
   return (
-    <View style={style.innerContainer}>
-      <View>
-        <Item data={data} images={images} image="temp" />
-      </View>
+    <View>
+      <Item data={data} images={images} image="temp" />
     </View>
   );
 };
 
-const Drop = ({data}: any) => {
+const Drop = ({ data }: any) => {
   return (
     <View>
       <Item data={data} images={images} image="drop" />
     </View>
-  )
-}
+  );
+};
 
-const Wind = ({data}: any) => {
+const Wind = ({ data }: any) => {
   return (
     <View>
       <Item data={data} images={images} image="wind" />
     </View>
-  )
-}
+  );
+};
 
-const Bulb = ({data}: any) => {
+const Bulb = ({ data }: any) => {
   return (
     <View>
       <Item data={data} images={images} image="bulb" />
     </View>
-  )
-}
+  );
+};
 
-const Users = ({data}: any) => {
+const Users = ({ data }: any) => {
   return (
     <View>
       <Item data={data} images={images} image="users" />
     </View>
-  )
-}
+  );
+};
 
-const WiFi = ({data}: any) => {
+const WiFi = ({ data }: any) => {
   return (
     <View>
       <Item data={data} images={images} image="wifi" />
     </View>
-  )
-}
+  );
+};
 
-export {Temprature, Drop, Wind, Bulb, Users, WiFi} ;
+export { Temprature, Drop, Wind, Bulb, Users, WiFi };
 
 const styles = StyleSheet.create({});
