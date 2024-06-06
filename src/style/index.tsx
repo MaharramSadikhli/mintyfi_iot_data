8d5bad3d8658af65733530ad85934b92944f2102
Muarem <s.muarem10@gmail.com>
2024-06-06 13:27:12 +0300

    created style binding

---
+++ src/style/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./container";
+import { RowContainer } from "./row-container";
+import { InnerContainer } from "./inner-container";
+import { DataText } from "./data-text";
+import { DataImage } from "./data-image";
+import { LogoContainer } from "./logo-container";
+import { LogoImage } from "./logo-img";
+
+export { Container, RowContainer, InnerContainer, DataText, DataImage, LogoContainer, LogoImage };
