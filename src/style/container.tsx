7e708e840dd94d93569c2408a6b99bbf7b7fe9e5
Muarem <s.muarem10@gmail.com>
2024-06-06 10:35:48 +0300

    created container style component

---
+++ src/style/container.tsx
@@ -0,0 +1,9 @@
+import { Colors } from "@/constants/Colors"
+import { ViewStyle } from "react-native"
+
+export const Container: ViewStyle = {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.light.background,
+}
