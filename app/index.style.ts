a7de9d8aa6f4f9d610ba9ba23b29d78e78091c48
Muarem <s.muarem10@gmail.com>
2024-06-06 17:22:05 +0300

    added list style

--- app/index.style.ts
+++ app/index.style.ts
@@ -1,11 +1,12 @@
 import { Colors } from "@/constants/Colors";
 import { StyleSheet } from "react-native";
-import { Container, RowContainer, InnerContainer, DataText, DataImage, LogoContainer, LogoImage } from "@/src/style";
+import { Container, RowContainer, InnerContainer, DataText, DataImage, LogoContainer, LogoImage, ListInnerContainer } from "@/src/style";
 
 export const style = StyleSheet.create({
   container: Container,
   rowContainer: RowContainer,
   innerContainer: InnerContainer,
+  listInnerContainer: ListInnerContainer,
   data_text: DataText,
   img: DataImage,
   logo_container: LogoContainer,
