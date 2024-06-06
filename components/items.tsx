a7518ca7b4fc870c3285960317315eaae219dbf9
Muarem <s.muarem10@gmail.com>
2024-06-06 10:11:11 +0300

    added scrolview

--- components/items.tsx
+++ components/items.tsx
@@ -1,18 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { style } from "@/app/index.style";
 import { Temprature, Drop, Wind, Bulb, Users, WiFi } from './items.data';
 
 const Items = ({data}: any) => {
   return (
-    <View style={style.rowContainer} >
+    <ScrollView horizontal={true} style={style.rowContainer} >
         <Temprature data={data} />
         <Drop data={data} />
         <Wind data={data} />
         <Bulb data={data} />
         <Users data={data} />
         <WiFi data={data} />
-      </View>
+      </ScrollView>
   )
 }
 
