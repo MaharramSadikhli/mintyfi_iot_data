d69ec1d98840b9bec6d5479b4e2ba8abe464f7ec
Muarem <s.muarem10@gmail.com>
2024-06-07 11:17:51 +0300

    added data control conditions

--- components/items.data.tsx
+++ components/items.data.tsx
@@ -16,92 +16,80 @@ const images = {
 const Temprature = ({ data }: any) => {
   const new_data = data[data.length - 1];
 
-  return (
-    <View style={style.innerContainer}>
-      <Item data={new_data.temp} images={images} image="temp" />
-    </View>
-  );
+  if (data.length > 0) {
+    return (
+      <View style={style.innerContainer}>
+        <Item data={new_data.temp} images={images} image="temp" />
+      </View>
+    );
+  }
 };
 
 const Drop = ({ data }: any) => {
   const new_data = data[data.length - 1];
 
-  return (
-    <View style={style.innerContainer}>
-      <Item data={new_data.hum} images={images} image="drop" />
-    </View>
-  );
+  if (data.length > 0) {
+    return (
+      <View style={style.innerContainer}>
+        <Item data={new_data.hum} images={images} image="drop" />
+      </View>
+    );
+  }
 };
 
 const Wind = ({ data }: any) => {
   const new_data = data[data.length - 1];
 
-  return (
-    <View style={style.innerContainer}>
-      <Item data={new_data.windVal} images={images} image="wind" />
-    </View>
-  );
+  if (data.length > 0) {
+    return (
+      <View style={style.innerContainer}>
+        <Item data={new_data.windVal} images={images} image="wind" />
+      </View>
+    );
+  }
 };
 
 const Bulb = ({ data }: any) => {
   const new_data = data[data.length - 1];
 
-  return (
-    <View style={style.innerContainer}>
-      <Item data={new_data.powerVal} images={images} image="bulb" />
-    </View>
-  );
+  if (data.length > 0) {
+    return (
+      <View style={style.innerContainer}>
+        <Item data={new_data.powerVal} images={images} image="bulb" />
+      </View>
+    );
+  }
 };
 
 const Users = ({ data }: any) => {
   const new_data = data[data.length - 1];
 
-  return (
-    <View style={style.innerContainer}>
-      <Item data={new_data.peopleVal} images={images} image="users" />
-    </View>
-  );
+  if (data.length > 0) {
+    return (
+      <View style={style.innerContainer}>
+        <Item data={new_data.peopleVal} images={images} image="users" />
+      </View>
+    );
+  }
 };
 
 const WiFi = ({ data }: any) => {
   const new_data = data[data.length - 1];
-  // console.log(new_data)
 
-  return (
-    <View style={style.innerContainer}>
-      {new_data.statusCon == "true" ? (
-        <Item data={new_data.statusCon} images={images} image="wifi" />
-      ) : (
-        <Item data={new_data.statusCon} images={images} image="nowifi" />
-      )}
-    </View>
-  );
+  if (data.length > 0) {
+    return (
+      <View style={style.innerContainer}>
+        {new_data.statusCon == "true" ? (
+          <Item data={new_data.statusCon} images={images} image="wifi" />
+        ) : (
+          <Item data={new_data.statusCon} images={images} image="nowifi" />
+        )}
+      </View>
+    );
+  }
 };
 
 export { Temprature, Drop, Wind, Bulb, Users, WiFi };
 
-const styles = StyleSheet.create({});
 
-// if there was a listing
 
-// const Temprature = ({ data }: any) => {
-//   return (
-//     <View>
-//       <View style={style.innerContainer}>
-//         <Image
-//           style={style.img}
-//           source={require("../src/img/temprature.png")}
-//         />
-//         <FlatList
-//           data={data}
-//           renderItem={({ item }) => (
-//             <View>
-//               <Text style={style.data_text}>{item.temp}</Text>
-//             </View>
-//           )}
-//           keyExtractor={(item) => item.idTemp}
-//         />
-//       </View>
-//     </View>
-//   );
-// };
