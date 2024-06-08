b8ffb308ba38693dfa5644307bf93829291f20ce
Muarem <s.muarem10@gmail.com>
2024-06-08 14:58:58 +0300

    crteated hook forr iot data

---
+++ hook/hook.iot.data.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import "./hook.iot.data.interface";
+
+const IotData = () => {
+  const [temperatureData, setTemperatureData] = useState<Temperature[]>([]);
+  const [humidityData, setHumidityData] = useState<Humidity[]>([]);
+  const [windData, setWindData] = useState<Wind[]>([]);
+  const [powerData, setPowerData] = useState<Power[]>([]);
+  const [peopleData, setPeopleData] = useState<People[]>([]);
+  const [connectionData, setConnectionData] = useState<Connection[]>([]);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/MaharramSadikhli/APIs/main/iot_ap_newi.json"
+      );
+      const json = await response.json();
+      setTemperatureData(json[0].temprature);
+      setHumidityData(json[1].humidity);
+      setWindData(json[2].wind);
+      setPowerData(json[3].power);
+      setPeopleData(json[4].people);
+      setConnectionData(json[5].connection);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return {
+    temperatureData,
+    humidityData,
+    windData,
+    powerData,
+    peopleData,
+    connectionData,
+  };
+};
+
+export default IotData;
