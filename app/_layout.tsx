b6dea4084e1b38eb3b08a89b738f4ca77ecbecdc
Muarem <s.muarem10@gmail.com>
2024-06-08 14:56:27 +0300

    optimized layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,17 @@
-import { Stack } from "expo-router";
-import React, {useEffect, useState} from "react";
-import * as  Font from 'expo-font';
-import { ActivityIndicator, View } from "react-native";
-import { style } from "./index.style";
+import React from "react";
+import { useFonts } from "expo-font";
+import StackComponent from "@/components/stack.component";
+import LoadingComponent from "@/components/loading.component";
+import ErrorComponent from "@/components/error.component";
 
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Sofia-Regular": require("../assets/fonts/Sofia-Regular.ttf"),
+    "ArchitectsDaughter-Regular": require("../assets/fonts/ArchitectsDaughter-Regular.ttf"),
+    "RedditMono-VariableFont_wght": require("../assets/fonts/RedditMono-VariableFont_wght.ttf"),
+  });
 
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    const loadFonts = async () => {
-      await Font.loadAsync({
-        'Sofia-Regular': require('../assets/fonts/Sofia-Regular.ttf'),
-        'ArchitectsDaughter-Regular': require('../assets/fonts/ArchitectsDaughter-Regular.ttf'),
-        'RedditMono-VariableFont_wght': require('../assets/fonts/RedditMono-VariableFont_wght.ttf'),
-      });
-
-      setFontsLoaded(true);
-
-    };
-
-    loadFonts();
-  }, []);
-
-  if (!fontsLoaded) {
-    return (
-      <View style={style.loadingContainer}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "Mintyfi IOT", headerTitleAlign:"center" }} />
-    </Stack>
-  );
+  if (fontError) return <ErrorComponent error={fontError.message} />;
+  if (!fontsLoaded) return <LoadingComponent />;
+  return <StackComponent />;
 }
-
